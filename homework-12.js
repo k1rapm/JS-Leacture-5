4c7907646330628ca62265843c0e112fc1e88f41
Adham Ismonov <kkirapm@gmail.com>
2024-01-05 13:45:52 +0500

    lecture5

---
+++ homework-12.js
@@ -0,0 +1,58 @@
+// 1
+
+// function t1(a) {
+//     return aect.values(a).length
+// }
+// console.log(t1({ a: "idiot", b: "idiot", c: "idiot" })
+// );
+
+// 2
+
+// function t2(a) {
+//   let a1 = "circle";
+//   let a2 = "semi-circle";
+//   let a3 = "triangle";
+//   let a4 = "square";
+//   let a5 = "pentagon";
+//   let a6 = "hexagon";
+//   let a7 = "heptagon";гее
+//   let a8 = "octagon";
+//   let a9 = "nonagon";
+//   let a10 = "decagon";
+//   if (a == 1) {
+//     return a1;
+//   } else if (a == 2) {
+//     return a2;
+//   } else if (a == 3) {
+//     return a3;
+//   } else if (a == 4) {
+//     return a4;
+//   } else if (a == 5) {
+//     return a5;
+//   } else if (a == 6) {
+//     return a6;
+//   } else if (a == 7) {
+//     return a7;
+//   } else if (a == 8) {
+//     return a8;
+//   } else if (a == 9) {
+//     return a9;
+//   } else if (a == 10) {
+//     return a10;
+//   }
+// }
+// console.log(t2(7));
+
+// 3
+
+// function t3(a,b) {
+//   if (a >=b.min && a<=b.max) return true
+//   else return false
+
+// }
+// console.log(t3(4, { min: 4, max: 5 }));
+
+
+function t7(a) {
+    Object.values
+  }
